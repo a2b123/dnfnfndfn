e605627479deee02c0b1b2bd678f9007187cf261
a2b123 <atbhati2@gmail.com>
2018-01-04 10:47:03 -0600

    Initial Commit

---
+++ NFLPlayoffs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NFLPlayoffs
+//
+//  Created by Amar Bhatia on 1/4/18.
+//  Copyright © 2018 AmarBhatia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
